75d0ae6d24876cdf343b1c6d4c4315165270ee6d
JD <dobiasovsky.honza@gmail.com>
2021-02-01 11:02:19 +0100

    Fix wrong document prefix used on successful id

--- obsahator.py
+++ obsahator.py
@@ -48,7 +48,7 @@ for doc_path in docs:
             print(errors)
             if not doc_dict['error']:
                 utility.rename_document(original_path=doc_dict['path'],
-                                new_name=config.FAIL_PREFIX+doc_dict['name'])
+                                new_name=config.FINISHED_PREFIX+doc_dict['name'])
 
         except IOError as e:
             print(f"{format(datetime.now(), '%Y-%m-%d %H:%M:%S')} ERROR (OBSAHATOR): Processing error in {doc_dict['name']} : {e}")
@@ -65,7 +65,7 @@ for doc_path in docs:
             print(errors)
             if not doc_dict['error']:
                 utility.rename_document(original_path=doc_dict['path'],
-                                new_name=config.FAIL_PREFIX+doc_dict['name'])
+                                new_name=config.FINISHED_PREFIX+doc_dict['name'])
 
         except IOError as e:
             print(f"{format(datetime.now(), '%Y-%m-%d %H:%M:%S')} ERROR (OBSAHATOR): Processing error in {doc_dict['name']} : {e}")
